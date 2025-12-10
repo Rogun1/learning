dcf5ee394dd1db8990c6e201a963737722c23d18
Raul M <moreanuraul1@gmail.com>
2025-12-10 19:02:23 +0200

    Session 7 - Homework ( NOT DONE )

---
+++ src/session7/HomeWork.java
@@ -0,0 +1,135 @@
+package session7;
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class HomeWork {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        //displayTodayDate();
+        //displayDateComponents(LocalDate.now());
+        //createSpecificDate();
+        //areDatesEqual();
+        //isTodaySpecificDate();
+        //displayCurrentTime();
+        //addWeeksToToday(2);
+        //findDayOfWeek(LocalDate.of(2024,8,19));
+        //daysBetween();
+        //isValidDate();
+    }
+
+    public static void displayTodayDate(){
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+    }
+
+    public static void displayDateComponents(LocalDate date){
+        System.out.println("Year: " + date.getYear());
+        System.out.println("Month: " + date.getMonth());
+        System.out.println("Day: " + date.getDayOfMonth());
+    }
+
+    public static void createSpecificDate(){
+        System.out.println(LocalDate.of(2025,8,19));
+    }
+
+    public static void areDatesEqual(){
+        System.out.println("Add dates in this format YYYY-MM-DD");
+        System.out.print("Add first date: ");
+        LocalDate date1 = LocalDate.parse(scanner.next());
+        System.out.print("Add second date: ");
+        LocalDate date2 = LocalDate.parse(scanner.next());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        boolean result = (date1.equals(date2));
+        System.out.println(result);
+    }
+
+    public static void isTodaySpecificDate(){
+        LocalDate date = LocalDate.now();
+        LocalDate dateToCheck = LocalDate.parse("2019-12-19");
+        System.out.println(date.equals(dateToCheck));
+    }
+
+    public static void displayCurrentTime(){
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+    }
+
+    public static void addWeeksToToday(int weeksToAdd){
+        LocalDate date = LocalDate.now();
+        System.out.println(date.plusWeeks(weeksToAdd));
+    }
+
+    public static void findDayOfWeek(LocalDate date){
+        System.out.println(date.getDayOfWeek());
+    }
+
+    public static void daysBetween(){
+        System.out.println("Add dates in this format YYYY-MM-DD");
+        System.out.print("Add first date: ");
+        LocalDate date1 = LocalDate.parse(scanner.next());
+        System.out.print("Add second date: ");
+        LocalDate date2 = LocalDate.parse(scanner.next());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        int result = date1.getDayOfMonth() - date2.getDayOfMonth();
+        System.out.println("Difference of " + result + " days.");
+    }
+
+    public static void isValidDate(){
+        System.out.print("Add a date: ");
+        String date = scanner.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        boolean dateIsValid = true;
+
+        if (date.length() == 10){
+            CHAR_SET:if (date.charAt(4) == '-' && date.charAt(7) == '-'){
+                String year = date.substring(0, 4);
+                //System.out.println(year);
+                for (int i = 0; i < year.length(); i++){
+                    if (!Character.isDigit(year.charAt(i))){
+                        //System.out.println("Invalid year");
+                        System.out.println(dateIsValid = false);
+                        break CHAR_SET;
+                    }
+                }
+                String month = date.substring(5, 7);
+                //System.out.println(month);
+                for (int i = 0; i < month.length(); i++){
+                    if (!Character.isDigit(month.charAt(i))){
+                        //System.out.println("Invalid month");
+                        System.out.println(dateIsValid = false);
+                        break CHAR_SET;
+                    }
+                }
+                String day = date.substring(8, 10);
+                //System.out.println(day);
+                for (int i = 0; i < day.length(); i++){
+                    if (!Character.isDigit(day.charAt(i))){
+                        //System.out.println("Invalid day");
+                        System.out.println(dateIsValid = false);
+                        break CHAR_SET;
+                    }
+                }
+
+                LocalDate parse = LocalDate.parse(date, formatter);
+                //System.out.println(parse);
+                System.out.println(dateIsValid);
+
+            }
+            else {
+                //System.out.println("Invalid Date 2");
+                System.out.println(dateIsValid = false);
+            }
+        }else{
+            //System.out.println("Invalid Date.");
+            System.out.println(dateIsValid = false);
+        }
+    }
+}

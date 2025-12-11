1d4ec750099ac4c66b69db061ce1ad9021142067
Raul M <moreanuraul1@gmail.com>
2025-12-11 16:31:42 +0200

    Session 7 - Homework(Done)

--- src/session7/HomeWork.java
+++ src/session7/HomeWork.java
@@ -1,11 +1,7 @@
 package session7;
 
-import javax.swing.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Formatter;
 import java.util.Scanner;
 
 public class HomeWork {
@@ -21,23 +17,21 @@ public class HomeWork {
         //findDayOfWeek(LocalDate.of(2024,8,19));
         //daysBetween();
         //isValidDate();
+        //elapsedTimeSince();
     }
 
     public static void displayTodayDate(){
         LocalDate localDate = LocalDate.now();
         System.out.println(localDate);
     }
-
     public static void displayDateComponents(LocalDate date){
         System.out.println("Year: " + date.getYear());
         System.out.println("Month: " + date.getMonth());
         System.out.println("Day: " + date.getDayOfMonth());
     }
-
     public static void createSpecificDate(){
         System.out.println(LocalDate.of(2025,8,19));
     }
-
     public static void areDatesEqual(){
         System.out.println("Add dates in this format YYYY-MM-DD");
         System.out.print("Add first date: ");
@@ -49,27 +43,22 @@ public class HomeWork {
         boolean result = (date1.equals(date2));
         System.out.println(result);
     }
-
     public static void isTodaySpecificDate(){
         LocalDate date = LocalDate.now();
         LocalDate dateToCheck = LocalDate.parse("2019-12-19");
         System.out.println(date.equals(dateToCheck));
     }
-
     public static void displayCurrentTime(){
         LocalTime time = LocalTime.now();
         System.out.println(time);
     }
-
     public static void addWeeksToToday(int weeksToAdd){
         LocalDate date = LocalDate.now();
         System.out.println(date.plusWeeks(weeksToAdd));
     }
-
     public static void findDayOfWeek(LocalDate date){
         System.out.println(date.getDayOfWeek());
     }
-
     public static void daysBetween(){
         System.out.println("Add dates in this format YYYY-MM-DD");
         System.out.print("Add first date: ");
@@ -81,8 +70,7 @@ public class HomeWork {
         int result = date1.getDayOfMonth() - date2.getDayOfMonth();
         System.out.println("Difference of " + result + " days.");
     }
-
-    public static void isValidDate(){
+    public static void  isValidDate(){
         System.out.print("Add a date: ");
         String date = scanner.nextLine();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -131,5 +119,24 @@ public class HomeWork {
             //System.out.println("Invalid Date.");
             System.out.println(dateIsValid = false);
         }
+    }
+    public static void elapsedTimeSince(){
+        System.out.print("Add a time as HH:mm:ss : ");
+        String time = scanner.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime parse = LocalTime.parse(time,formatter);
+        System.out.println(time);
+
+        LocalTime localtime = LocalTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println(localtime.format(timeFormatter));
+
+        int hours = localtime.getHour() - parse.getHour();
+        int minutes = localtime.getMinute() - parse.getMinute();
+        int seconds = localtime.getSecond() - parse.getSecond();
+        System.out.println("The time span given as " + hours + " hours," + minutes + " minutes," + seconds + " seconds.");
+         //System.out.println(parse.getClass());
+
+    }
     }
 }

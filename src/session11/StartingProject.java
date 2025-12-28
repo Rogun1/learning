277f749dc4d537f144b33bd08bcea35b63b4b0cc
Raul M <moreanuraul1@gmail.com>
2025-12-28 21:04:31 +0200

    Session - 11 (Emails done)

--- src/session11/StartingProject.java
+++ src/session11/StartingProject.java
@@ -12,7 +12,7 @@ public class StartingProject {
         Roles roleAdmin = new Roles(Roles.admin);
         Roles roleManager = new Roles(Roles.manager);
 
-        //Every person has automatically an employee role
+        //Every person has an employee role
         Person person1 = new Person("Raul", "Mrn", roleEmployee);
         Person person2 = new Person("Alin", "Alc", roleEmployee);
         Person person3 = new Person("Bursuc", "Zns", roleEmployee);
@@ -22,17 +22,24 @@ public class StartingProject {
         Person person7 = new Person("Ccuas", "Wasdy", roleEmployee);
         Person person8 = new Person("Nbqw", "Wqbs", roleEmployee);
 
+        //List for every role
         List<Person> employers = new ArrayList<>(Arrays.asList());
         List<Person> admins = new ArrayList<>(Arrays.asList());
         List<Person> managers = new ArrayList<>(Arrays.asList());
 
+        //Construction
         Construction building = new Construction();
+
+        //Resources
         Resource brick = new Resource("brick", 2, 50);
         Resource wood = new Resource("wood", 100, 500);
         Resource metal = new Resource("metal", 100, 100);
 
-        //person1 is the manager,admin and employee
-        //Simulating that we have accounts
+        //Send emails.
+        Email email = new Email();
+
+        //Just the person1 is the manager,admin and employee
+        //Simulating that we have accounts, each account is atleast a employee
         person1.addRole(roleManager);
         person1.addRole(roleAdmin);
         managers.add(person1);
@@ -49,74 +56,83 @@ public class StartingProject {
 
         person1.personProfile();
 
+        //I could just do if statement without for loop.
+        //I should have an authenticator and based on the account ID by example, to check if is a manager .
+        for (Person person: employers){
+            if (person.hasRole("manager")){
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("You are a manager.");
+                System.out.print("See manager Things? y/n : ");
+                String answer = scanner.next();
 
-        //There should be a user authentication
-        // so i can assign a role and based on that to do the verification if person can see employers
-        Scanner scanner = new Scanner(System.in);
+                if (answer.equals("y")){
+                    System.out.println("Employers: ");
+                    for (Person p: employers){
+                        System.out.print(p.fullName() + ", ");
+                    };
+                    //Creating new teams with a leader
+                    Teams team1 = new Teams(person1);
+                    Teams team2 = new Teams(person3);
+                    Teams team3 = new Teams(person4);
+                    List<Teams> teams = new ArrayList<>(Arrays.asList());
 
-        if (person1.hasRole("manager")){
-            System.out.println("You are a manager.");
-            System.out.print("See manager Things? y/n : ");
-            String answer = scanner.next();
+                    teams.add(team1);
+                    teams.add(team2);
+                    teams.add(team3);
+                    team1.addMembers(person2);
+                    team1.addMembers(person5);
+                    team1.addMembers(person6);
+                    team2.addMembers(person4);
+                    team2.addMembers(person7);
+                    team2.addMembers(person8);
+                    team3.addMembers(person1);
+                    team3.addMembers(person2);
+                    team3.addMembers(person3);
 
-            if (answer.equals("y")){
-                System.out.println("Employers: ");
-                for (Person p: employers){
-                    System.out.print(p.fullName() + ", ");
-                };
-                //Creating new teams with a leader
-                Teams team1 = new Teams(person1);
-                Teams team2 = new Teams(person3);
-                Teams team3 = new Teams(person4);
-                List<Teams> teams = new ArrayList<>(Arrays.asList());
+                    System.out.println();
+                    System.out.println();
+                    checkTeam(teams);
+                    team1.setLeader(person2);
+                    checkTeam(teams);
+                    System.out.println();
 
-                teams.add(team1);
-                teams.add(team2);
-                teams.add(team3);
-                team1.addMembers(person2);
-                team1.addMembers(person5);
-                team1.addMembers(person6);
-                team2.addMembers(person4);
-                team2.addMembers(person7);
-                team2.addMembers(person8);
-                team3.addMembers(person1);
-                team3.addMembers(person2);
-                team3.addMembers(person3);
+                    // setting a resource to building ( check if already exists )
+                    building.setAvailabeResources(brick);
+                    building.setAvailabeResources(wood);
+                    building.setAvailabeResources(metal);
+                    // getting the resources from building
+                    System.out.println("Available resources: ");
+                    building.getAvailabeResources();
+                    System.out.println();
 
-                System.out.println();
-                System.out.println();
-                checkTeam(teams);
-                team1.setLeader(person2);
-                checkTeam(teams);
+                    brick.setQuantity(30);
+                    System.out.println();
+                    System.out.print("Available resources: ");
+                    building.getAvailabeResources();
+                    System.out.println();
+                    System.out.print("Quantity needed for " + brick.getName() + " : " + brick.getQuantityNeeded());
+                    System.out.println();
+                    brick.setQuantityNeeded(120);
+                    System.out.println();
+                    System.out.print("Quantity needed for " + brick.getName() + " : " + brick.getQuantityNeeded());
+                    System.out.println();
+                    System.out.print("Available resources: ");
+                    building.getAvailabeResources();
+                    System.out.println();
 
-                System.out.println();
-
-                // setting a resource to building ( check if already exists )
-                building.setAvailabeResources(brick);
-                building.setAvailabeResources(wood);
-                building.setAvailabeResources(metal);
-                // getting the resources from building
-                System.out.println("Available resources: ");
-                building.getAvailabeResources();
-                System.out.println();
-
-                brick.setQuantity(30);
-                System.out.println();
-                System.out.print("Available resources: ");
-                building.getAvailabeResources();
-                System.out.println();
-                System.out.print("Quantity needed for " + brick.getName() + " : " + brick.getQuantityNeeded());
-                System.out.println();
-                brick.setQuantityNeeded(120);
-                System.out.println();
-                System.out.print("Quantity needed for " + brick.getName() + " : " + brick.getQuantityNeeded());
-                System.out.println();
-                System.out.print("Available resources: ");
-                building.getAvailabeResources();
+                    System.out.print("Do you want to send an email to employers? y/n:");
+                    answer = scanner.next();
+                    if (answer.equals("y")){
+                        email.write();
+                        email.send(employers);
+                    }else {
+                        System.out.println("No emails to send today.");
+                    }
+                }
             }
-        }
-        else {
-            System.out.println("You have no access to employers");
+            else {
+                System.out.println(person.fullName() + " you have no access to employers");
+            }
         }
     }
     public static void checkTeam(List<Teams> team){
